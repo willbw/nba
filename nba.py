6bc23333690e8690170a7b73b6e4cf633aad4fcb
Will <willwright90@gmail.com>
2018-03-30 00:59:27 +1100

    Box score functionality added, work on interface

--- nba.py
+++ nba.py
@@ -2,19 +2,31 @@
 import json
 import urllib.request
 import sys
+import os
 from datetime import date, timedelta, datetime
 from dateutil import parser
 from time import strftime
 
-class Team(object):
-  tri = ''
-  score = 0
-  win_loss = ''
+dir = os.path.dirname(os.path.abspath(__file__))
 
+with open(dir + '/players.json', 'r') as file:
+  data = json.load(file)['league']['standard']
+  players = {}
+  for p in data:
+    players[p['personId']] = p['firstName'] + ' ' + p['lastName'] 
+
+with open(dir + '/teams.json', 'r') as file:
+  data = json.load(file)['league']['standard']
+  teams = {}
+  for t in data:
+    teams[t['teamId']] = t['fullName']
+
+class Team(object):
   def __init__(self, obj):
     self.tri = obj['triCode']
     self.score = obj['score']
     self.win_loss = obj['win'] + '-' + obj['loss']
+    self.teamId = obj['teamId']
 
 class Game(object):
   def __init__(self, obj):
@@ -58,10 +70,36 @@ class Game(object):
 
   def get_boxscore(self):
     url = f'http://data.nba.net/10s/prod/v1/{self.date}/{self.id}_boxscore.json'
-    print(url)
     with urllib.request.urlopen(url) as response:
         data = json.load(response)
-    self.box = data
+    players = data['stats']['activePlayers']
+    return players
+
+  def print_boxscore(self):
+    WIDTH = 117
+    if not self.box:
+      self.box = self.get_boxscore
+    print("-" * WIDTH)
+    print(f"{teams[self.v.teamId]:^117}")
+    print("-" * WIDTH)
+    print(f"{'Name':^25} | {'Min':^5} | {'FG':^5} | {'3P':^5} | {'FT':^5} | OREB | DREB | REB | AST | STL | BLK | TO | PF | +/- | PTS ")
+    print("-" * WIDTH)
+    for p in self.box:
+      if p['teamId'] == self.v.teamId:
+        print(f"{players[p['personId']]:<25} | {p['min']:>5} | {p['fgm']:>2}/{p['fga']:<2} | {p['tpm']:>2}/{p['tpa']:<2} | {p['ftm']:>2}/{p['fta']:<2} | "
+        f"{p['offReb']:>4} | {p['defReb']:>4} | {p['totReb']:>3} | {p['assists']:>3} | {p['steals']:>3} | {p['blocks']:>3} | {p['turnovers']:>2} | "
+        f"{p['pFouls']:>2} | {p['plusMinus']:>3} | {p['points']:>3}")
+    print("-" * WIDTH)
+    print(f"{teams[self.h.teamId]:^117}")
+    print("-" * WIDTH)
+    print(f"{'Name':^25} | {'Min':^5} | {'FG':^5} | {'3P':^5} | {'FT':^5} | OREB | DREB | REB | AST | STL | BLK | TO | PF | +/- | PTS ")
+    print("-" * WIDTH)
+    for p in self.box:
+      if p['teamId'] == self.h.teamId:
+        print(f"{players[p['personId']]:<25} | {p['min']:>5} | {p['fgm']:>2}/{p['fga']:<2} | {p['tpm']:>2}/{p['tpa']:<2} | {p['ftm']:>2}/{p['fta']:<2} | "
+        f"{p['offReb']:>4} | {p['defReb']:>4} | {p['totReb']:>3} | {p['assists']:>3} | {p['steals']:>3} | {p['blocks']:>3} | {p['turnovers']:>2} | "
+        f"{p['pFouls']:>2} | {p['plusMinus']:>3} | {p['points']:>3}")
+    
   
   def __str__(self):
     s = '{:5}{:4}-{:>4}{:>5}'.format(self.h.tri, self.h.score, self.v.score, self.v.tri)
@@ -75,18 +113,12 @@ class Game(object):
     s += '   ' + self.nugget
     return s
   
+def print_scores(games):
+  for i, g in enumerate(games, 1):
+    print()
+    print(f'GAME {i}')
+    print(g)
 
-    
-#  print_game(self):
-#   print('\n{:5}{:4}-{:>4}{:>5}'
-#   .format(g.h.tri, g.h.score, g.v.score, g.v.tri), end='')
-#   if g.period:
-#     print('{:>5}{:>6}'.format(g.period, g.clock), end = '')
-#   else:
-#     print('   ' + g.start, end = '')
-#   print('\n{:<9} {:>9}'.format(g.h.win_loss, g.v.win_loss))
-
-# def main():
 adj = 0
 TIME_AHEAD = 11
 if len(sys.argv) > 1: 
@@ -108,9 +140,27 @@ print(f'Game day: {day}')
 
 games = [Game(g) for g in data['games']] 
 
-for g in games:
-  print()
-  print(g)
+print_scores(games)
 
-# if __name__ == '__main__':
-#   main()
+for g in games:
+  try:
+    g.box = g.get_boxscore()
+  except:
+    pass
+
+
+while True:
+  print()
+  print('Enter a game number to see the box score or "s" to see the scores again')
+  action = input(' > ')
+  try:
+    if action == 's':
+      print_scores(games)
+    else:
+      action = int(action)
+      games[action-1].print_boxscore()
+  except:
+    if action - 1 >= len(games): 
+      print('Invalid selection.')
+    else:
+      print('Unable to get boxscore.')
